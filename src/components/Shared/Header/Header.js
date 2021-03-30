cb0617b04cf50cd88d90f949c1abfa78a59865b9
KimDongHoon <kdh888_2000@naver.com>
2021-03-30 10:09:56 +0900

    .

---
+++ src/components/Shared/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="header">
+    </div>
+  )
+}
+
+export default Header;
